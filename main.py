a85fc33362787557e129baa53f2a31f4a4298149
dghddgh <d3069241@gmail.com>
2025-05-29 15:14:49 +0700

    commit for second function

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 def first():
     print("Привет! Добро пожаловать")
+    
+def add_numbers(a, b):
+    result = a + b
+    print(f"Сумма {a} и {b} равна {result}")
+    return result
 
 def main():
     first()
+    add_numbers(5,7)
 
 if __name__ == "__main__":
     main()
