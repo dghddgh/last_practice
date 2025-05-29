481f013623445f1f333f3808d43728be354170f7
dghddgh <d3069241@gmail.com>
2025-05-29 15:17:44 +0700

    commit for third function

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ def add_numbers(a, b):
     result = a + b
     print(f"Сумма {a} и {b} равна {result}")
     return result
-
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
 def main():
     first()
     add_numbers(5,7)
+    print(f"Факториал числа 5: {factorial(5)}")
+    
+
 
 if __name__ == "__main__":
     main()
